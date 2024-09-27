1a8311f97cebb4aa0e1a70ef140624d8b4ebb132
Velin Dankov <velin80@web.de>
2024-09-27 13:26:13 +0300

    Add one extra movie

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -3,6 +3,7 @@ import routes from "../routes.js";
 
 function expressCfg(app) {
   app.use(express.static("public"));
+  app.use(express.urlencoded({extended: false}));
 
   app.use(routes);
 }
--- src/data/movieData.js
+++ src/data/movieData.js
@@ -1,19 +1,30 @@
-import fs  from "fs/promises";
+import fs from "fs/promises";
 import path from "path";
+const dbPath = path.resolve("./src/db.json");
 
 async function getDb() {
-    const dbPath = path.resolve('./src/db.json');
-    const db = await fs.readFile(dbPath, {encoding: "utf-8"});
-    const data = JSON.parse(db)
-    return data
-    
+  const jsonResult = await fs.readFile(dbPath, { encoding: "utf-8" });
+  const data = JSON.parse(jsonResult);
+  return data;
+}
+function saveDb(data) {
+  return fs.writeFile(dbPath, JSON.stringify(data, {}, 2));
 }
 
-async function getMovies() {
-    const db = await getDb()
-    return  db.movies;
+async function create(movieData) {
+  const db = await getDb();
+
+  db.movies.push(movieData);
+
+  return saveDb(db);
+}
+
+async function getAll() {
+  const db = await getDb();
+  return db.movies;
 }
 
 export default {
-    getMovies
-}
+  getAll,
+  create
+};
