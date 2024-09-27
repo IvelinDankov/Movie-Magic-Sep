7f487f5f8eca7d9329807186986e58af582d9b57
Velin Dankov <velin80@web.de>
2024-09-27 09:42:59 +0300

    set up express

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+   res.send('just work')
+});
+
+app.listen(5000, () => console.log(`Server is listening on local host 5000 ... `))
